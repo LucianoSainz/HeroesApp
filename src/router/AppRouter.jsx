838720b5d2b80b997a6c6f795256446bad89bbfc
LucianoSainz <danielsainz28@gmail.com>
2022-07-26 10:44:29 +0200

    order folder and navbar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom"
-import { DcPage } from "../heroes/pages/DCPage"
-import { MarvelPage } from "../heroes/pages/MarvelPage"
+import { DcPage, MarvelPage } from "../heroes"
+
 import { LoginPage } from "../auth/pages/LoginPage"
+import { Navbar } from "../ui"
 
 
 export const AppRouter = () => {
     return (
         <>
+
+        <Navbar /> 
+
           <Routes>
             <Route path="marvel" element={<MarvelPage />} />
             <Route path="dc" element={<DcPage />} />
