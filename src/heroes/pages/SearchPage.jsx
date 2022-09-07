95de7032dd7d0ea706f150613d2c5226ee8d23a6
LucianoSainz <danielsainz28@gmail.com>
2022-09-07 11:35:37 +0200

    worked with search section

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,26 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm'
 import { HeroCard } from '../components'
+import queryString from 'query-string'
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = ''} = queryString.parse(location.search)
+
+  const {searchText, onInputChange} = useForm({
+    searchText: ''
+  });
+
+  const onSearchSubmit = (e) => {
+   e.preventDefault();
+   if (searchText.trim().length <= 1) return;
+
+    navigate(`?q=${ searchText }`)
+  }
+
     return(
         <>
           <h1>Search</h1>
@@ -10,8 +30,9 @@ export const SearchPage = () => {
           <div className="col-5">
             <h4>Searching</h4>
             <hr />
-            <form>
-              <input type="text" placeholder="Search a hero" className="form-control" name="searchText" autoComplete="off" />
+
+            <form onSubmit={onSearchSubmit}>
+              <input type="text" placeholder="Search a hero" className="form-control" name="searchText" autoComplete="off" value={searchText} onChange={onInputChange} />
               <button className="btn btn-outline-primary mt-1">Search</button>
             </form>
           </div>
@@ -25,7 +46,7 @@ export const SearchPage = () => {
             </div>
 
             <div className="alert alert-danger">
-              There's no Results
+              No hero with <b>{ q }</b>
             </div>
 
             {/*<HeroCard {...hero} /> */}
